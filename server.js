a5cf9f975e37bc9bdd0c65cd79a1e79e957673ba
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-08-17 18:04:43 +0200

    Init default basic project

--- server.js
+++ server.js
@@ -18,3 +18,49 @@
  */
 
 'use strict'; // https://www.w3schools.com/js/js_strict.asp
+
+// append process.env object by some system variables ( ./.env )
+require('dotenv').config();
+
+// add global fetch extension
+import('node-fetch').then(({ default: fetch }) => {
+    global.fetch = fetch;
+});
+
+// imports
+const fs = require('node:fs');
+const path = require('node:path');
+const express = require('express');
+const compression = require('compression');
+const serveFavicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const port = process.env.PORT;
+
+// create the express application
+const app = express();
+
+// enable module caching by adding a new global require function
+app.modules = {};
+global.moduleRequire = (mod) => {
+    if (app.modules[mod]) return app.modules[mod];
+    app.modules[mod] = require(mod);
+    return app.modules[mod];
+};
+
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('json spaces', 4);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// send a 404 at each request if route not found
+app.all('*', async (req, res) => res.status(404).json({ error: true, message: 'not found', code: 404 }));
+// Finally create listener for given port or default 8080
+app.listen(port || 8080, () => {
+    console.log('[ PRODUCTION ] » WEB Server is now running on Port: ' + port);
+});
